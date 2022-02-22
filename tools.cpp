960d8561fc0fcc1aeef50115789609c420ea097c
ChexuanQiao <61403491+ChexuanQiao@users.noreply.github.com>
2022-02-22 10:51:32 +0000

    Create tools.cpp

---
+++ tools.cpp
@@ -0,0 +1,6 @@
+  startMillis = millis();  //initial start time
+  currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+  ....
+    
+    
+    
