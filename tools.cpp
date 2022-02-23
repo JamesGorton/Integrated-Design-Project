553b1396b70e83b800052ca39cef8127e205a17f
ChexuanQiao <61403491+ChexuanQiao@users.noreply.github.com>
2022-02-23 09:59:25 +0000

    Update tools.cpp

--- tools.cpp
+++ tools.cpp
@@ -3,4 +3,7 @@
   ....
     
     
-    
+int number = intQueue.dequeue();    // Will return number 1 and remove it from the queue
+int number = intQueue.getHead();    // Will return number 123 but leave it still in the queue
+int number = intQueue.dequeue();    // Will return number 123 and remove it from the queue
+intQueue.enqueue(1);    // Adds number 1 to the queue
